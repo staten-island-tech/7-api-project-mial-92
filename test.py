b05184a8c51f1f860fd6175f98450b7ad126f932
mial-92 <mialin0170@gmail.com>
2025-12-05 10:07:04 -0500

    Update test.py

--- test.py
+++ test.py
@@ -1,4 +1,4 @@
-import requests
+""" import requests
 
 def getchar(character):
     response = requests.get(f"https://api.disneyapi.dev/character{character.lower()}")
@@ -13,5 +13,21 @@ def getchar(character):
         "charallies": data["allies"],
         "charenemies": data["enemies"]
     }
-charinfo = getchar("Herucles")
+charinfo = getchar("Hercules")
+print(charinfo)
+ """
+import requests
+
+def getchar(character):
+    response = requests.get(f"https://api.disneyapi.dev/character")
+    if response.status_code != 200:
+        print("Error. Cannot fetch data.")
+        return None
+    
+    data = response.json()
+    for i in range(len(data)):
+        if data[i]["name"] == character:
+            return data[i]["name"]
+    
+charinfo = getchar("Hercules")
 print(charinfo)
