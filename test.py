9e8efa0aed3d1409e6cf3332f9f8aa86119260fa
mial-92 <mialin0170@gmail.com>
2025-12-08 10:16:43 -0500

    Update test.py

--- test.py
+++ test.py
@@ -1,33 +1,17 @@
 import requests
 
-def getchar(character):
-    response = requests.get(f"https://api.disneyapi.dev/character{character.lower()}")
-    if response.status_code != 200:
+def getfruit(fruit):
+    fruit = requests.get(f"https://www.fruityvice.com/api/fruit/{fruit.lower()}")
+    if fruit.status_code != 200:
         print("Error. Cannot fetch data.")
         return None
     
-    data = response.json()
+    data = fruit.json()
     return{
-        "filmname": data["films"],
-        "charname": data["name"],
-        "charallies": data["allies"],
-        "charenemies": data["enemies"]
+        "fruit name": data["name"],
+        "fruit genus": data["genus"],
+        "fruit family": data["family"],
+        "fruit nutrients": data["nutritions"]
     }
-charinfo = getchar("Hercules")
-print(charinfo)
-
-""" import requests
-
-def getchar(character):
-    response = requests.get(f"https://api.disneyapi.dev/character")
-    if response.status_code != 200:
-        print("Error. Cannot fetch data.")
-        return None
-    
-    data = response.json()
-    for i in range(len(data)):
-        if data[i]["name"] == character:
-            return data[i]["name"]
-    
-charinfo = getchar("Hercules")
-print(charinfo) """
+fruitinfo = getfruit("banana")
+print(fruitinfo)
