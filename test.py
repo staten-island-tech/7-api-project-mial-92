51d5c6331a31cb626a390b0eb43eb1cd538ac721
mial-92 <mialin0170@gmail.com>
2025-12-09 10:17:09 -0500

    Update test.py

--- test.py
+++ test.py
@@ -1,4 +1,4 @@
-import requests
+""" import requests
 
 def getfruit(fruit):
     fruit = requests.get(f"https://www.fruityvice.com/api/fruit/{fruit.lower()}")
@@ -13,5 +13,18 @@ def getfruit(fruit):
         "fruit family": data["family"],
         "fruit nutrients": data["nutritions"]
     }
+fruitinfo = getfruit("banana")
+print(fruitinfo) """
+
+import requests
+
+def getfruit(fruit):
+    fruit = requests.get(f"https://www.fruityvice.com/api/fruit/{fruit.lower()}")
+    if fruit.status_code != 200:
+        print("Error. Cannot fetch data.")
+        return None
+    
+    data = fruit.json()
+    return data
 fruitinfo = getfruit("banana")
 print(fruitinfo)
